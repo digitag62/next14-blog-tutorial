ac408fe6b314d04a55cca48de28a49bd67c036ae
Rangga <rangga@gurrly.com>
2024-03-28 13:40:18 +0800

    Stage 3: site header

---
+++ config/site.ts
@@ -0,0 +1,14 @@
+export const siteConfig = {
+	name: "RanggaBlog",
+	url: "https://example.com",
+	description: "Nextjs 14 blog using velite, tailwind, and shadcn",
+	author: "Rangga D. Permadi",
+	links: {
+		twitter: "https://twitter.com/rgdmprmd",
+		github: "https://github.com/rgdmprmd",
+		instagram: "https://instagram.com/rgdmprmd",
+		personalSite: "https://ranggadpermadi-tailwinds.netlify.app",
+	},
+};
+
+export type SiteConfig = typeof siteConfig;
