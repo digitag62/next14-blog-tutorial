41b84cc7ac1764ab07dca510b6a23e91343ca1ef
digitag62 <144676268+digitag62@users.noreply.github.com>
2024-04-02 03:46:09 +0000

    Stage 9: Code blocks styling & mdx plugins

--- app/blog/[...slug]/page.tsx
+++ app/blog/[...slug]/page.tsx
@@ -2,36 +2,42 @@ import { posts } from "#site/content";
 import { MDXComponents } from "@/components/mdx-components";
 import { notFound } from "next/navigation";
 
+import "@/styles/mdx.css";
+
 interface PostPageProps {
-	params: {
-		slug: string[];
-	};
+  params: {
+    slug: string[];
+  };
 }
 
 const getPostFromParams = async (params: PostPageProps["params"]) => {
-	const slug = params?.slug?.join("/");
-	const post = posts.find((post) => post.slugAsParams === slug);
+  const slug = params?.slug?.join("/");
+  const post = posts.find((post) => post.slugAsParams === slug);
 
-	return post;
+  return post;
 };
 
-export const generateStaticParams = async (): Promise<PostPageProps["params"][]> => {
-	return posts.map((post) => ({ slug: post.slugAsParams.split("/") }));
+export const generateStaticParams = async (): Promise<
+  PostPageProps["params"][]
+> => {
+  return posts.map((post) => ({ slug: post.slugAsParams.split("/") }));
 };
 
 const PostPage = async ({ params }: PostPageProps) => {
-	const post = await getPostFromParams(params);
-	if (!post || !post.published) notFound();
+  const post = await getPostFromParams(params);
+  if (!post || !post.published) notFound();
 
-	return (
-		<article className="container py-6 prose dark:prose-invert max-w-3xl mx-auto">
-			<h1 className="mb-2">{post.title}</h1>
-			{post.description ? <p className="text-xl mt-0 text-muted-foreground">{post.description}</p> : null}
-			<hr className="my-4" />
+  return (
+    <article className="container py-6 prose dark:prose-invert max-w-3xl mx-auto">
+      <h1 className="mb-2">{post.title}</h1>
+      {post.description ? (
+        <p className="text-xl mt-0 text-muted-foreground">{post.description}</p>
+      ) : null}
+      <hr className="my-4" />
 
-			<MDXComponents code={post.body} />
-		</article>
-	);
+      <MDXComponents code={post.body} />
+    </article>
+  );
 };
 
 export default PostPage;
