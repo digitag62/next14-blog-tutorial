ea47978416e13e20c1c9e2e364786f5e25f8cb78
digitag62 <144676268+digitag62@users.noreply.github.com>
2024-04-03 05:00:43 +0000

    Stage 17: Fix name in home

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
       <section className="space-y-6 pb-8 pt-6 md:pb-12 md:mt-10 lg:py-32">
         <div className="container flex flex-col text-center gap-4">
           <h1 className="text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-black text-balance">
-            Hello, I&apos;m Noverio
+            Hello, I&apos;m Rangga
           </h1>
           <p className="max-w-[42rem] mx-auto text-muted-foreground sm:text-xl text-balance">
             Welcome to my blog template. Built using Tailwind, Shadcn, Velite
